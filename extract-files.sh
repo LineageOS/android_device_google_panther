CalyxOS/device_google_oriole

Add extract-files.sh helper script
Change-Id: Ie5f4c6ab8debf1a2b11db45b0cb6957e2539ac80
(cherry picked from commit 40bffd6293c1d33acf397fd338b7f04c4d13b156)

extract-files.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+MY_DIR="$(cd "$(dirname "${0}")"; pwd -P)"
+
+"${MY_DIR}/../raviole/oriole/extract-files.sh" "$@"

LineageOS/android_device_google_panther

extract-files.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+MY_DIR="$(cd "$(dirname "${0}")"; pwd -P)"
+
+"${MY_DIR}/../pantah/panther/extract-files.sh" "$@"